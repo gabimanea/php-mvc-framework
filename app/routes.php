01810869bcc74c9547219371dc7e451ed4927e4f
Pocsan Titi Ionut Catalin <pocsanjr@gmail.com>
2016-01-01 15:59:37 -0800

    Update routes.php

--- app/routes.php
+++ app/routes.php
@@ -3,3 +3,7 @@
 $app->get('/', 'HomeController@index', 'home');
 
 $app->get('/about', 'HomeController@about', 'about');
+
+$app->get('/contact', function() {
+  echo "You can contact me at pocsanjr@gmail.com";
+}, "contact");
