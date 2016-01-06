1d38571d4e393a7be9ca360a5fb8224244e10915
Pocsan Titi Ionut Catalin <pocsanjr@gmail.com>
2016-01-06 09:36:21 -0800

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -2,6 +2,10 @@
 
 $app = new System\Application();
 
+// if the framework files and folders are within a folder in your local server, in my case, mvc.
+// you have to set the base path.
+$app->getInstance('router')->setBasePath('mvc/public/');
+
 require_once BASE_PATH . 'app/routes.php';
 
 $app->run();
